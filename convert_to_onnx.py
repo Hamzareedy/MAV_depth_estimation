2a6358638876c92b28ee345d24fbeb99ac786008
pietbcm <pietbouwens.cm@gmail.com>
2025-03-04 20:22:09 +0100

    Add ONNX conversion

---
+++ convert_to_onnx.py
@@ -0,0 +1,28 @@
+import torch
+import config
+import model
+from torch.quantization import quantize_dynamic
+
+def export_to_onnx(model_path, model_id, use_quantization=False):
+    depth_model = model.DepthModel()
+    depth_model.load_state_dict(torch.load(model_path))
+    depth_model.eval()
+
+    # Optimize by reducing precision (32bit float to 8bit int) to reduce model size and improve inference speed
+    if use_quantization:
+        depth_model = quantize_dynamic(depth_model, dtype=torch.qint8)
+
+    # Export to ONNX
+    # TODO: Infer the image height and width somehow, don't hardcode it
+    dummy_input = torch.randn(1, config.config["input_channels"], 240, 520)
+
+    onnx_model_path = config.config["save_model_path"] + "/onnx_models/model_" + str(model_id) + ".onnx"
+    torch.onnx.export(depth_model, dummy_input, onnx_model_path)
+
+    print(f"Exported model_{model_id} to ONNX in {onnx_model_path}")
+
+if __name__ == "__main__":
+    model_id = 0
+    model_path = config.config["save_model_path"] + "/model_" + str(model_id) + ".pth"
+
+    export_to_onnx(model_path, model_id, True)
