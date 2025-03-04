2804f5e6948bf45e5205e7576aa5effe606f563b
pietbcm <pietbouwens.cm@gmail.com>
2025-03-04 22:10:31 +0100

    Add eval function

--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -8,7 +8,8 @@ def export_to_onnx(model_path, model_id, use_quantization=False):
     depth_model.load_state_dict(torch.load(model_path))
     depth_model.eval()
 
-    # Optimize by reducing precision (32bit float to 8bit int) to reduce model size and improve inference speed
+    # Optimize for embedded CPU's by reducing precision (32 bit float to 8 bit int) to reduce 
+    # model size and improve inference speed
     if use_quantization:
         depth_model = quantize_dynamic(depth_model, dtype=torch.qint8)
 
@@ -16,13 +17,13 @@ def export_to_onnx(model_path, model_id, use_quantization=False):
     # TODO: Infer the image height and width somehow, don't hardcode it
     dummy_input = torch.randn(1, config.config["input_channels"], 240, 520)
 
-    onnx_model_path = config.config["save_model_path"] + "/onnx_models/model_" + str(model_id) + ".onnx"
+    onnx_model_path = config.config["save_model_path"] + f"/onnx_models/model_{model_id}.onnx"
     torch.onnx.export(depth_model, dummy_input, onnx_model_path)
 
     print(f"Exported model_{model_id} to ONNX in {onnx_model_path}")
 
 if __name__ == "__main__":
     model_id = 0
-    model_path = config.config["save_model_path"] + "/model_" + str(model_id) + ".pth"
+    model_path = config.config["save_model_path"] + f"/model_{model_id}.pth"
 
     export_to_onnx(model_path, model_id, True)
--- dataset.py
+++ dataset.py
@@ -6,6 +6,7 @@ from PIL import Image
 from torchvision import transforms as T
 from torch.utils.data import Dataset, DataLoader
 import config
+import random
 
 
 class DepthDataset(Dataset):
@@ -56,3 +57,20 @@ def load_train_val_dataset():
         num_workers=config.config["num_workers"]
         )
     return train_dataloader, val_dataloader
+
+def load_eval_dataset(num_imgs):
+    '''
+        Load num_imgs number of images for evaluation
+    '''
+    dataset = DepthDataset()
+    random_indices = random.sample(range(len(dataset)), num_imgs)
+    eval_dataset = torch.utils.data.Subset(dataset, random_indices)
+
+    eval_data_loader = DataLoader(
+        eval_dataset,
+        batch_size=1,
+        shuffle=False,
+        num_workers=config.config["num_workers"]
+        )
+    
+    return eval_data_loader
